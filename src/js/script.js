94099d2762447f3b349d5ff838dbad785636eb79
LiveG <hi@liveg.tech>
2018-06-10 14:55:27 +0100

    Add buttons and inputs

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,15 @@
+var mouseUse = `
+    .menuButton:active {
+        background-color: rgba(0, 0, 0, 0.2);
+    }
+`;
+
+var keyboardUse = `
+    .menuButton:focus {
+        background-color: rgba(0, 0, 0, 0.2);
+    }
+`;
+
 $(function() {
     $("div[data-page]").css("display", "none");
     $("div[data-page-default]").css("display", "unset");
@@ -9,5 +21,25 @@ $(function() {
         $(this).addClass("selected");
         $("div[data-page]").css("display", "none");
         $("div[data-page='" + $(this).attr("data-for") + "']").css("display", "unset");
+    });
+
+    $("div[data-for]").attr("tabindex", "0");
+    $(".menuButton").attr("tabindex", "0");
+
+    $("*").keypress(function (e) {
+        var key = e.which;
+
+        if (key == 13) {
+            $(this).click();
+            return false;  
+        }
+    });
+
+    $("*").hover(function() {
+        $("#toggleStyle").text(mouseUse);
+    });
+
+    $("*").keydown(function() {
+        $("#toggleStyle").text(keyboardUse);
     });
 });
